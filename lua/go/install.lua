c5848462a48fc7dcabe65635524b7e599ab54ec7
David Zhang <crispgm@gmail.com>
2021-03-03 21:45:50 +0800

    fix: should not run on cwd, which add to go.mod

--- lua/go/install.lua
+++ lua/go/install.lua
@@ -7,15 +7,13 @@ local output = require('go.output')
 
 -- Install binaries
 function M.install_binaries()
-    local cwd = vim.fn.expand('%:p:h')
-
     print('[GoInstallBinaries] Starting...')
     for _, tool in ipairs(config.tools) do
         vim.api.nvim_command(string.format('echom "[GoInstallBinaries] Installing %s: %s ..."', tool.name, tool.repo))
         local results, code = Job:new({
             command = 'go',
             args = { 'get', tool.repo },
-            cwd = cwd,
+            cwd = '~',
         }):sync(30000)
         if code == 0 then
             output.show_success('GoInstallBinaries', string.format('Installed %s', tool.name))
