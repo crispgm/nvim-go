7a36dbcf27162b9bee82a10b43e17f2a54846116
David Zhang <crispgm@gmail.com>
2021-03-05 13:44:23 +0800

    chore: format init.lua

--- lua/go/init.lua
+++ lua/go/init.lua
@@ -1,12 +1,12 @@
 local go = setmetatable({}, {
-  __index = function(t, k)
-    local ok, val = pcall(require, string.format('go.%s', k))
-    if ok then
-      rawset(t, k, val)
-    end
+    __index = function(t, k)
+        local ok, val = pcall(require, string.format('go.%s', k))
+        if ok then
+            rawset(t, k, val)
+        end
 
-    return val
-  end
+        return val
+    end
 })
 
 local vim = vim
