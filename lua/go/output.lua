33f31597a93b86ae63271fe753119d290c988c32
David Zhang <crispgm@gmail.com>
2021-03-09 23:58:35 +0800

    feat: auto close popup window when buf closed

--- lua/go/output.lua
+++ lua/go/output.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 local vim = vim
-local Popup = require('popup')
+local popup = require('popup')
 
 function M.show_success(prefix, msg)
     local succ = 'Success'
@@ -43,11 +43,24 @@ local function calc_popup_size()
     return top, width
 end
 
+function M.close_popup(win_id, bufnr)
+    if vim.api.nvim_buf_is_valid(bufnr)
+        and not vim.api.nvim_buf_get_option(bufnr, 'buflisted') then
+        vim.cmd(string.format("silent! bdelete! %s", bufnr))
+    end
+
+    if not vim.api.nvim_win_is_valid(win_id) then
+        return
+    end
+
+    vim.api.nvim_win_close(win_id, true)
+end
+
 function M.popup_job_result(results)
     local bufnr = vim.api.nvim_create_buf(false, false)
     vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, results)
     local top, width = calc_popup_size()
-    Popup.create(bufnr, {
+    local popup_win, _ = popup.create(bufnr, {
         line = top,
         col = 2,
         border = { 1, 1, 1, 1 },
@@ -56,6 +69,18 @@ function M.popup_job_result(results)
         minwidth = width,
         highlight = 'Normal',
     })
+    vim.api.nvim_win_set_option(popup_win, 'wrap', false)
+    vim.api.nvim_win_set_option(popup_win, 'winhl', 'Normal:GoTestResult')
+
+    local popup_bufnr = vim.api.nvim_win_get_buf(popup_win)
+    local on_buf_leave = string.format(
+    [[  autocmd BufLeave <buffer> ++nested ++once :silent lua require('go.output').close_popup(%s, %s)]],
+    popup_win, popup_bufnr)
+
+    vim.cmd([[augroup NvimGoPopup]])
+    vim.cmd([[  autocmd!]])
+    vim.cmd(on_buf_leave)
+    vim.cmd([[augroup END]])
 end
 
 return M
